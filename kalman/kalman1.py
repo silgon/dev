6c6b673bb085efe946cebfd354cbac10fe2baf4b
silgon <silgon3200@gmail.com>
2013-05-20 22:27:41 +0200

    some changes in linear kalman

--- kalman/kalman1.py
+++ kalman/kalman1.py
@@ -15,26 +15,25 @@ class KalmanFilterLinear:
     self.A = _A                      # State transition matrix.
     self.B = _B                      # Control matrix.
     self.H = _H                      # Observation matrix.
-    self.current_state_estimate = _x # Initial state estimate.
-    self.current_prob_estimate = _P  # Initial covariance estimate.
+    self.x = _x                      # Initial state estimate.
+    self.P = _P                      # Initial covariance estimate.
     self.Q = _Q                      # Estimated error in process.
     self.R = _R                      # Estimated error in measurements.
   def GetCurrentState(self):
-    return self.current_state_estimate
-  def Step(self,control_vector,measurement_vector):
+    return self.x
+  def Step(self,u,z): # u:control vector, z: measurement
     #---------------------------Prediction step-----------------------------
-    predicted_state_estimate = self.A * self.current_state_estimate + self.B * control_vector
-    predicted_prob_estimate = (self.A * self.current_prob_estimate) * np.transpose(self.A) + self.Q
+    xhat = self.A * self.x + self.B * u # predicted state
+    Phat = (self.A * self.P) * np.transpose(self.A) + self.Q # covariance estimate
     #--------------------------Observation step-----------------------------
-    innovation = measurement_vector - self.H*predicted_state_estimate
-    innovation_covariance = self.H*predicted_prob_estimate*np.transpose(self.H) + self.R
+    ytil = z - self.H*xhat # innovation: measurement residual
+    S = self.H*Phat*np.transpose(self.H) + self.R # innovation: residual covariance 
     #-----------------------------Update step-------------------------------
-    kalman_gain = predicted_prob_estimate * np.transpose(self.H) * np.linalg.inv(innovation_covariance)
-    self.current_state_estimate = predicted_state_estimate + kalman_gain * innovation
+    K = Phat * np.transpose(self.H) * np.linalg.inv(S) # kalman gain
+    self.x = xhat + K * ytil
     # We need the size of the matrix so we can make an identity matrix.
-    size = self.current_prob_estimate.shape[0]
-    # eye(n) = nxn identity matrix.
-    self.current_prob_estimate = (np.eye(size)-kalman_gain*self.H)*predicted_prob_estimate
+    size = self.P.shape[0]
+    self.P = (np.eye(size)-K*self.H)*Phat
 
 class Voltmeter:
   def __init__(self,_truevoltage,_noiselevel):
