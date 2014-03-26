4316bb4e88694bee90e86459fdf7a8d61bd2d478
silgon <silgon3200@gmail.com>
2014-03-26 16:06:33 +0100

    - some comments on data analysis course

--- ecourses/danalysis/l1_1.m
+++ ecourses/danalysis/l1_1.m
@@ -6,7 +6,7 @@ n=128; % points in the function
 x2=linspace(-L/2,L/2,n+1);
 x=x2(1:n); % we get rid of the last point because the last point is the same as the first
 
-k=(2*pi/L)*[0:n/2-1 -n/2:-1];
+k=(2*pi/L)*[0:n/2-1 -n/2:-1]; % normalize to our domain (2*pi meaning frequency), and shifting because of the butterfly calculations happening in the fft
 
 u=exp(-x.^2); % my gaussian function
 
@@ -16,7 +16,7 @@ plot(fftshift(k),abs(fftshift(ut))); % shifting the stuff
 
 
 %% second part with a sech ecuation
-L=20; % Domain
+L=20; % Domain. As in example for the gibbs phenomenon you can reduce de domain (like to 4). You'll see oscillations between the jump, since now the function is not periodic and we're violating that principle
 n=128; % points in the function
 
 x2=linspace(-L/2,L/2,n+1);
--- ecourses/danalysis/l1_2.m
+++ ecourses/danalysis/l1_2.m
@@ -6,24 +6,24 @@ n=512; % points in the function
 
 t2=linspace(-T/2,T/2,n+1);
 t=t2(1:n); % we get rid of the last point because the last point is the same as the first
-k=(2*pi/T)*[0:n/2-1 -n/2:-1];
+k=(2*pi/T)*[0:n/2-1 -n/2:-1]; % normalize to our domain (2*pi meaning frequency), and shifting because of the butterfly calculations happening in the fft
 ks=fftshift(k); % shift the function for the fourier transformation
 
 u=sech(t); % function
 ut=fft(u); % fourier transform
 
 % add noise
-noise=10;
+noise=20;
 utn=ut+noise*(randn(1,n)+i*randn(1,n)); % fourier transform with white noise
 un=ifft(utn);
 
-filter=exp(-k.^2); % filter
+filter=exp(-(k+0).^2); % filter
 utnf=filter.*utn; % filtering the signal
 unf=ifft(utnf); % inverse fft
 
 % subplot(2,1,1), plot(t,u,'k',t,un,'m');
 % subplot(2,1,2), plot(ks,abs(fftshift(ut))/max(abs(fftshift(ut))),'k',ks,abs(fftshift(utn))/max(abs(fftshift(utn))),'m');
-subplot(2,1,1), plot(t,un,'m', t, unf,'g',t,0*t+.5,'g');
+subplot(2,1,1), plot(t,un,'m', t, unf,'g',t,0*t+.5,'k:');
 axis([-15 15 0 1])
 subplot(2,1,2), plot(ks,abs(fftshift(utn))/max(abs(fftshift(utn))),'m',...
     ks,abs(fftshift(filter))/max(abs(fftshift(filter))),'b',...
