d23b15606888772fbfa32d2ab02eba2ff48115eb
silgon <silgon3200@gmail.com>
2014-06-05 17:16:00 +0200

    Vector Intersection Analisis

---
+++ vector_intersection.m
@@ -0,0 +1,22 @@
+clc; clf; clear all;
+% values a and b in [x,y,theta]
+a=[10 19 -pi/4];
+b=[3 4 pi/4];
+
+x1=a(1); y1=a(2); theta1=a(3);
+x2=b(1); y2=b(2); theta2=b(3);
+
+A=[x2-x1 y2-y1]';
+B=[sin(theta1) -sin(theta2); cos(theta1) -cos(theta2)];
+
+% calculating parameters for projection
+s=inv(B)*A, % matrix method
+z=(-(x2-x1)*cos(theta2)+(y2-y1)*sin(theta2))/sin(theta2-theta1) % by hand method
+
+%% a,b
+%% x1+s(1)*sin(theta1), y1+s(1)*cos(theta1)
+hold on
+plot(a(1),a(2),'r+')
+plot(b(1),b(2),'r+')
+plot(x1+s(1)*sin(theta1), y1+s(1)*cos(theta1),'r+')
+plot(x1+z*sin(theta1), y1+z*cos(theta1),'ko') % same projection with by hand result
