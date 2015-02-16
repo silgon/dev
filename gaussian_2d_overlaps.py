2da2ff338811db8030cc2b8f50819ad207cba2fe
silgon <silgon3200@gmail.com>
2015-02-16 23:47:31 +0100

    gradients in overlaps

--- gaussian_2d_overlaps.py
+++ gaussian_2d_overlaps.py
@@ -5,7 +5,8 @@ sig_rx = 2
 sig_lx = 1
 sig_y = 2
 
-side = np.arange(-10, 10, .05)
+step = .1
+side = np.arange(-10, 10, step)
 size = len(side)
 X_base, Y_base = np.meshgrid(side, side)
 def proxemics(x=0, y=0, th=0):
@@ -31,10 +32,34 @@ def rot(theta):
     return np.array([[np.cos(theta), -np.sin(theta)],
                      [np.sin(theta), np.cos(theta)]])
 
+
+# Function for every point
 Z1 = proxemics(2,2, np.pi)
 Z2 = proxemics(-2,2, 0)
 Z3 = proxemics(0,2, np.pi/2.)
+
+# Sum
 Z = Z1+Z2+Z3
+plt.subplot(2,2,1)
+plt.title("Sum")
 plt.pcolormesh(X_base, Y_base, Z)
 plt.colorbar()
+
+# Gradients
+plt.subplot(2,2,2)
+plt.title("Gradient Quiver")
+gy,gx = np.gradient(Z, step, step)
+plt.quiver(X_base, Y_base, gy, gx)
+
+# x-gradient
+plt.subplot(2,2,3)
+plt.title("Gradient x axis")
+plt.pcolormesh(X_base, Y_base, gx)
+plt.colorbar()
+# y-gradient
+plt.subplot(2,2,4)
+plt.title("Gradient y axis")
+plt.pcolormesh(X_base, Y_base, gy)
+plt.colorbar()
+
 plt.show()
