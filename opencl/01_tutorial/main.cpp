36f7aa9d165111f9150e36f03c1ea58c008b2420
silgon <silgon3200@gmail.com>
2015-09-19 18:19:37 +0200

    opencl: added cmake compatibility and Makefile modified

--- opencl/01_tutorial/main.cpp
+++ opencl/01_tutorial/main.cpp
@@ -15,26 +15,37 @@
 #define DEVICE CL_DEVICE_TYPE_DEFAULT
 #endif
 
+#ifndef CL_SOURCE_DIR
+#define CL_SOURCE_DIR ""
+#endif
+
 //------------------------------------------------------------------------------
 
 #define LENGTH (10)    // length of vectors a, b, and c
 
 inline std::string loadProgram(std::string input)
 {
-    std::ifstream stream(input.c_str());
+    std::string filepath;
+    // if created with make file
+    if(CL_SOURCE_DIR == "")
+        filepath = input;
+    // if created with CMakeLists.txt file
+    else
+        filepath = static_cast<std::string>(CL_SOURCE_DIR) +"/" + input;
+
+    std::ifstream stream(filepath.c_str());
     if (!stream.is_open()) {
-        std::cout << "Cannot open file: " << input << std::endl;
+        std::cout << "Cannot open file: " << filepath << std::endl;
         exit(1);
     }
-
     return std::string(std::istreambuf_iterator<char>(stream),
                        (std::istreambuf_iterator<char>()));
 }
 
 int main(void) {
-    std::vector<float> h_a(LENGTH);                // a vector 
-    std::vector<float> h_b(LENGTH);                // b vector 	
-    std::vector<float> h_c (LENGTH);   // c vector (result)
+    std::vector<float> v_a(LENGTH);                // a vector 
+    std::vector<float> v_b(LENGTH);                // b vector 	
+    std::vector<float> v_c (LENGTH);   // c vector (result)
 
     cl::Buffer d_a;                       // device memory used for the input  a vector
     cl::Buffer d_b;                       // device memory used for the input  b vector
@@ -43,8 +54,8 @@ int main(void) {
     // Fill vectors a and b with random float values
     int count = LENGTH;
     for(int i = 0; i < count; i++) {
-        h_a[i]  = i;
-        h_b[i]  = i;
+        v_a[i]  = i;
+        v_b[i]  = i;
     }
 
     try {
@@ -62,8 +73,8 @@ int main(void) {
  
         cl::make_kernel<cl::Buffer, cl::Buffer, cl::Buffer> vadd(program, "vadd");
 
-        d_a = cl::Buffer(context, h_a.begin(), h_a.end(), true);
-        d_b = cl::Buffer(context, h_b.begin(), h_b.end(), true);
+        d_a = cl::Buffer(context, v_a.begin(), v_a.end(), true);
+        d_b = cl::Buffer(context, v_b.begin(), v_b.end(), true);
         d_c = cl::Buffer(context, CL_MEM_READ_WRITE, sizeof(float) * LENGTH);
 
         vadd(
@@ -74,10 +85,10 @@ int main(void) {
              d_b,
              d_c);
 
-        cl::copy(queue, d_c, h_c.begin(), h_c.end());
+        cl::copy(queue, d_c, v_c.begin(), v_c.end());
         std::cout << "c: ";
         for (int i = 0; i < count; i++) {
-            std::cout << h_c[i] << ", ";
+            std::cout << v_c[i] << ", ";
         }
         std::cout << "\n";
     }
