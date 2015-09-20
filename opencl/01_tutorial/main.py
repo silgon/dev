cf658078b19ab787ae14ce17cba4e3f870e0b5af
silgon <silgon3200@gmail.com>
2015-09-20 12:48:05 +0200

    opencl: python class changed

--- opencl/01_tutorial/main.py
+++ opencl/01_tutorial/main.py
@@ -1,44 +1,66 @@
 # from Adventures in OpenCL Part1 to PyOpenCL
 
 import pyopencl as cl
-import numpy
+import numpy as np
 
 class CL:
+    """
+    This class have all the configuration of to load and run
+    an openCL program
+    """
     def __init__(self):
         self.ctx = cl.create_some_context()
         self.queue = cl.CommandQueue(self.ctx)
+        self.mf = cl.mem_flags
 
     def loadProgram(self, filename):
-        #read in the OpenCL source file as a string
+        """
+        The file with the opencl program is loaded
+        """
+        # read in the OpenCL source file as a string
         f = open(filename, 'r')
         fstr = "".join(f.readlines())
-        # print fstr  # print cl program
-        #create the program
+        # create the program
         self.program = cl.Program(self.ctx, fstr).build()
 
-    def popCorn(self):
-        mf = cl.mem_flags
+    def vectToBuffer(self, vector):
+        """ Converts numpy array to opencl array"""
+        return cl.Buffer(self.ctx, self.mf.READ_ONLY | self.mf.COPY_HOST_PTR, hostbuf=vector)
 
+    def bufferToVect(self, buff, vect):
+        """ Converts opencl array to numpy array"""
+        cl.enqueue_read_buffer(self.queue, buff, vect).wait()
+        
+    def outBuffer(self, nbytes):
+        """creates output for opencl """
+        return cl.Buffer(self.ctx, self.mf.WRITE_ONLY, nbytes)
+    
+    
+    def runAlgo(self):
+        """
+        The program is created here
+        """
         #initialize client side (CPU) arrays
-        self.a = numpy.array(range(10), dtype=numpy.float32)
-        self.b = numpy.array(range(10), dtype=numpy.float32)
+        a = np.array(range(10), dtype=np.float32)
+        b = np.array(range(10), dtype=np.float32)
 
         #create OpenCL buffers
-        self.a_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.a)
-        self.b_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.b)
-        self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, self.b.nbytes)
+        a_buf = self.vectToBuffer(a)
+        b_buf = self.vectToBuffer(b)
+        dest_buf = self.outBuffer(b.nbytes)
+        # execute program
+        self.program.vadd(self.queue, a.shape, None,
+                          a_buf, b_buf, dest_buf)
+        c = np.empty_like(a)
+        self.bufferToVect(dest_buf, c)
 
-    def execute(self):
-        self.program.vadd(self.queue, self.a.shape, None, self.a_buf, self.b_buf, self.dest_buf)
-        c = numpy.empty_like(self.a)
-        cl.enqueue_read_buffer(self.queue, self.dest_buf, c).wait()
-        print "a", self.a
-        print "b", self.b
+        # print result
+        print "a", a
+        print "b", b
         print "c", c
 
 if __name__ == "__main__":
     example = CL()
     example.loadProgram("vector_add.cl")
-    example.popCorn()
-    example.execute()
+    example.runAlgo()
 
