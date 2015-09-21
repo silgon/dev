cc6de1b8d3d4d5b81d434af8dee327ebc7a96c0c
silgon <silgon3200@gmail.com>
2015-09-21 17:01:04 +0200

    pyopencl: not sure if working

--- opencl/02_matrix_mult/main.py
+++ opencl/02_matrix_mult/main.py
@@ -24,21 +24,40 @@ class myAlgo(CL):
         a_buf = self.vectToBuffer(h_A)
         b_buf = self.vectToBuffer(h_B)
         dest_buf = self.outBuffer(h_A.nbytes)
+
+        np.set_printoptions(threshold='nan')
         # execute program
         mmul = self.program.mmul
         mmul.set_scalar_arg_dtypes([np.int32, None, None, None])
         mmul(self.queue, (N, N), None,
              N, a_buf, b_buf, dest_buf)
-
         print "First problem solved"
+
+        h_C = np.empty_like(h_A)
+        self.bufferToVect(dest_buf, h_C)
+        print "{}".format(h_C)
 
         localmem = cl.LocalMemory(np.dtype(np.float32).itemsize * N)
         mmul2 = self.program.mmul2
         mmul2.set_scalar_arg_dtypes([np.int32, None, None, None, None])
         mmul2(self.queue, (N,), (1024/16,),
               N, a_buf, b_buf, dest_buf, localmem)
-
         print "Second problem solved"
+
+        h_C = np.empty_like(h_A)
+        self.bufferToVect(dest_buf, h_C)
+        print "{}".format(h_C)
+
+        blocksize = 16
+        A_block = cl.LocalMemory(np.dtype(np.float32).itemsize*blocksize**2)
+        B_block = cl.LocalMemory(np.dtype(np.float32).itemsize*blocksize**2)
+        mmul3 = self.program.mmul3
+        mmul3.set_scalar_arg_dtypes([np.int32, None, None, None, None, None])
+        mmul3(self.queue, (N, N), (blocksize, blocksize),
+              N, a_buf, b_buf, dest_buf, A_block, B_block)
+
+        print "Third problem solved"
+
 
         h_C = np.empty_like(h_A)
         self.bufferToVect(dest_buf, h_C)
