b7925e56d2a136ecd3d611a36a6db3455e636351
silgon <silgon3200@gmail.com>
2015-12-08 00:28:30 +0100

    cpp hdf5 working with eigen for writing (not readying yet)

--- c/hdf5/test_hdf5.cpp
+++ c/hdf5/test_hdf5.cpp
@@ -23,58 +23,87 @@
 #include <iostream>
 #include <string>
 #include <H5Cpp.h>
+#include <vector>
+#include <Eigen/Eigen>
+
+typedef Eigen::Matrix<double, Eigen::Dynamic,
+                      Eigen::Dynamic, Eigen::RowMajor> MatrixXdR;
 
 const H5std_string	FILE_NAME("h5tutr_dset.h5");
-const H5std_string	DATASET_NAME("dset");
-const int	 NX = 4;                     // dataset dimensions
-const int	 NY = 6;
-const int	 RANK = 2;
+const H5std_string	DATASET_NAME("dset2");
 
 int main (void)
 {
+    unsigned int seed = 3;
+    std::srand(seed);
     // Try block to detect exceptions raised by any of the calls inside it
-    try
-        {
-            // Turn off the auto-printing when failure occurs so that we can
-            // handle the errors appropriately
-            H5::Exception::dontPrint();
+    try {
+        // Turn off the auto-printing when failure occurs so that we can
+        // handle the errors appropriately
+        H5::Exception::dontPrint();
 
-            // Create a new file using the default property lists. 
-            H5::H5File file(FILE_NAME, H5F_ACC_TRUNC);
+        // Create a new file using the default property lists.
+        H5::H5File file(FILE_NAME, H5F_ACC_TRUNC);
+        // H5::H5File file(FILE_NAME, H5F_ACC_RDWR);
 
-            // Create the data space for the dataset.
-            hsize_t dims[2];               // dataset dimensions
-            dims[0] = NX;
-            dims[1] = NY;
-            H5::DataSpace dataspace(RANK, dims);
+        int rank = 2; // dataset dimensions
+        hsize_t dims[2];
+        hsize_t * dims_ptr;
+        dims_ptr = dims;
 
-            // Create the dataset.      
-            H5::DataSet dataset = file.createDataSet(DATASET_NAME,
-                                                     H5::PredType::STD_I32BE,
-                                                     dataspace);
+        dims[0] = 10;
+        dims[1] = 2;
 
-        }  // end of try block
+        // generate random data
+        double *dataptr;
+        MatrixXdR A(dims[0], dims[1]);
+        A = Eigen::MatrixXd::Random(A.rows(), A.cols());
+        std::cout << A << "\n";
+        dataptr = A.data();
+        H5::DataSpace *dataspace = new H5::DataSpace(rank, dims);
 
+        // Create the dataset
+        H5::DataSet *dataset = new H5::DataSet(file.createDataSet(DATASET_NAME,
+                                      H5::PredType::NATIVE_DOUBLE, *dataspace));
+        dataset->write(dataptr, H5::PredType::NATIVE_DOUBLE);
+
+        file.close();
+        delete dataspace;
+        delete dataset;
+        delete dataptr;
+
+        file.openFile(FILE_NAME, H5F_ACC_RDONLY);
+        dataset = new H5::DataSet(file.openDataSet(DATASET_NAME));
+        dataspace = new H5::DataSpace(dataset->getSpace());
+        std::cout << "old rank: "<<rank << "\t old dims: "
+                  << dims[0] << ","<<dims[1] << "\n";
+        rank = dataspace->getSimpleExtentNdims();
+        rank = dataspace->getSimpleExtentDims(dims_ptr);
+        std::cout << "new rank: "<<rank << "\t new dims: "
+                  << dims[0] << ","<<dims[1] << "\n";
+        file.close();
+        delete dataspace;
+        delete dataset;
+        delete dataptr;
+
+    }  // end of try block
     // catch failure caused by the H5File operations
-    catch(H5::FileIException error)
-        {
-            error.printError();
-            return -1;
-        }
+    catch(H5::FileIException error) {
+        error.printError();
+        return -1;
+    }
 
     // catch failure caused by the DataSet operations
-    catch(H5::DataSetIException error)
-        {
-            error.printError();
-            return -1;
-        }
+    catch(H5::DataSetIException error) {
+        error.printError();
+        return -1;
+    }
 
     // catch failure caused by the DataSpace operations
-    catch(H5::DataSpaceIException error)
-        {
-            error.printError();
-            return -1;
-        }
+    catch(H5::DataSpaceIException error) {
+        error.printError();
+        return -1;
+    }
 
     return 0;  // successfully terminated
 }
