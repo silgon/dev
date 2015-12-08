03a18323fee3398726521bf506c36f1a0e5acd93
silgon <silgon3200@gmail.com>
2015-12-08 13:02:29 +0100

    automatic creation, automatic data type while reading

--- c/hdf5/test_hdf5.cpp
+++ c/hdf5/test_hdf5.cpp
@@ -34,23 +34,28 @@ const H5std_string	DATASET_NAME("dset1");
 
 int main (void)
 {
-    unsigned int seed = 2;
+    unsigned int seed = 1;
     std::srand(seed);
     // Turn off the auto-printing when failure occurs so that we can
     // handle the errors appropriately
-    H5::Exception::dontPrint();
+    // H5::Exception::dontPrint();
 
     // Create a new file using the default property lists.
     // H5::H5File file;
-    // file.openFile(FILE_NAME, H5F_ACC_RDWR | H5F_ACC_CREAT);
-    H5::H5File file(FILE_NAME, H5F_ACC_TRUNC);
+    // file->openFile(FILE_NAME, H5F_ACC_RDWR | H5F_ACC_CREAT);
+    H5::H5File *file;
+    try {
+        file = new H5::H5File(FILE_NAME, H5F_ACC_RDWR);
+    } catch(const H5::FileIException& e) {
+        file = new H5::H5File(FILE_NAME, H5F_ACC_TRUNC);
+    }
     int rank = 2; // dataset dimensions
     hsize_t dims[2];
     hsize_t * dims_ptr;
     dims_ptr = dims;
 
-    dims[0] = 10;
-    dims[1] = 2;
+    dims[0] = 12;
+    dims[1] = 3;
 
     // generate random data
     double *dataptr;
@@ -59,38 +64,39 @@ int main (void)
     std::cout << A << "\n";
     dataptr = A.data();
     H5::DataSpace *dataspace = new H5::DataSpace(rank, dims);
-
+    H5::DSetCreatPropList  *plist = new  H5::DSetCreatPropList;
+    // plist->setDeflate(6);
     // Create the dataset
-    H5::DataSet *dataset = new H5::DataSet(file.createDataSet(DATASET_NAME,
-                                   H5::PredType::NATIVE_DOUBLE, *dataspace));
+    H5::DataSet *dataset = new H5::DataSet(file->createDataSet(DATASET_NAME,
+                                H5::PredType::NATIVE_DOUBLE, *dataspace, *plist));
     dataset->write(dataptr, H5::PredType::NATIVE_DOUBLE);
-
-    file.close();
+    file->close();
     delete dataspace;
     delete dataset;
     dataptr = NULL;
 
     // Reading file
-    file.openFile(FILE_NAME, H5F_ACC_RDONLY);
-    dataset = new H5::DataSet(file.openDataSet(DATASET_NAME));
+    file->openFile(FILE_NAME, H5F_ACC_RDONLY);
+    dataset = new H5::DataSet(file->openDataSet(DATASET_NAME));
     dataspace = new H5::DataSpace(dataset->getSpace());
     std::cout << "old rank: "<<rank << "\t old dims: "
               << dims[0] << ","<<dims[1] << "\n";
     rank = dataspace->getSimpleExtentNdims();
     rank = dataspace->getSimpleExtentDims(dims_ptr);
-    double data[dims[0]*dims[1]];
-    dataptr = data;
+    // double data[dims[0]*dims[1]];
+    dataptr = new double [dims[0]*dims[1]];
     std::cout << "new rank: "<<rank << "\t new dims: "
               << dims[0] << ","<<dims[1] << "\n";
-    dataset->read(dataptr, H5::PredType::NATIVE_DOUBLE);
-    // dataset->read(dataptr, dataset->getTypeClass());
+
+    dataset->read(dataptr, dataset->getDataType());
+
     A = Eigen::Map<MatrixXdR>(dataptr, dims[0], dims[1]);
-    file.close();
+    file->close();
     std::cout << A << "\n";
 
     delete dataspace;
     delete dataset;
+    delete dataptr;
     dataptr = NULL;
-
     return 0;  // successfully terminated
 }
