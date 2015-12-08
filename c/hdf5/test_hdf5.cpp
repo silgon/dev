40641d07b65baa40bfde884a80369fa06e656d8f
silgon <silgon3200@gmail.com>
2015-12-08 10:03:59 +0100

    test_hdf5 writing and reading to eigen matrix working, further tests need to be done

--- c/hdf5/test_hdf5.cpp
+++ c/hdf5/test_hdf5.cpp
@@ -30,80 +30,67 @@ typedef Eigen::Matrix<double, Eigen::Dynamic,
                       Eigen::Dynamic, Eigen::RowMajor> MatrixXdR;
 
 const H5std_string	FILE_NAME("h5tutr_dset.h5");
-const H5std_string	DATASET_NAME("dset2");
+const H5std_string	DATASET_NAME("dset1");
 
 int main (void)
 {
-    unsigned int seed = 3;
+    unsigned int seed = 2;
     std::srand(seed);
-    // Try block to detect exceptions raised by any of the calls inside it
-    try {
-        // Turn off the auto-printing when failure occurs so that we can
-        // handle the errors appropriately
-        H5::Exception::dontPrint();
+    // Turn off the auto-printing when failure occurs so that we can
+    // handle the errors appropriately
+    H5::Exception::dontPrint();
 
-        // Create a new file using the default property lists.
-        H5::H5File file(FILE_NAME, H5F_ACC_TRUNC);
-        // H5::H5File file(FILE_NAME, H5F_ACC_RDWR);
+    // Create a new file using the default property lists.
+    // H5::H5File file;
+    // file.openFile(FILE_NAME, H5F_ACC_RDWR | H5F_ACC_CREAT);
+    H5::H5File file(FILE_NAME, H5F_ACC_TRUNC);
+    int rank = 2; // dataset dimensions
+    hsize_t dims[2];
+    hsize_t * dims_ptr;
+    dims_ptr = dims;
 
-        int rank = 2; // dataset dimensions
-        hsize_t dims[2];
-        hsize_t * dims_ptr;
-        dims_ptr = dims;
+    dims[0] = 10;
+    dims[1] = 2;
 
-        dims[0] = 10;
-        dims[1] = 2;
+    // generate random data
+    double *dataptr;
+    MatrixXdR A(dims[0], dims[1]);
+    A = Eigen::MatrixXd::Random(A.rows(), A.cols());
+    std::cout << A << "\n";
+    dataptr = A.data();
+    H5::DataSpace *dataspace = new H5::DataSpace(rank, dims);
 
-        // generate random data
-        double *dataptr;
-        MatrixXdR A(dims[0], dims[1]);
-        A = Eigen::MatrixXd::Random(A.rows(), A.cols());
-        std::cout << A << "\n";
-        dataptr = A.data();
-        H5::DataSpace *dataspace = new H5::DataSpace(rank, dims);
+    // Create the dataset
+    H5::DataSet *dataset = new H5::DataSet(file.createDataSet(DATASET_NAME,
+                                   H5::PredType::NATIVE_DOUBLE, *dataspace));
+    dataset->write(dataptr, H5::PredType::NATIVE_DOUBLE);
 
-        // Create the dataset
-        H5::DataSet *dataset = new H5::DataSet(file.createDataSet(DATASET_NAME,
-                                      H5::PredType::NATIVE_DOUBLE, *dataspace));
-        dataset->write(dataptr, H5::PredType::NATIVE_DOUBLE);
+    file.close();
+    delete dataspace;
+    delete dataset;
+    dataptr = NULL;
 
-        file.close();
-        delete dataspace;
-        delete dataset;
-        dataptr = NULL;
+    // Reading file
+    file.openFile(FILE_NAME, H5F_ACC_RDONLY);
+    dataset = new H5::DataSet(file.openDataSet(DATASET_NAME));
+    dataspace = new H5::DataSpace(dataset->getSpace());
+    std::cout << "old rank: "<<rank << "\t old dims: "
+              << dims[0] << ","<<dims[1] << "\n";
+    rank = dataspace->getSimpleExtentNdims();
+    rank = dataspace->getSimpleExtentDims(dims_ptr);
+    double data[dims[0]*dims[1]];
+    dataptr = data;
+    std::cout << "new rank: "<<rank << "\t new dims: "
+              << dims[0] << ","<<dims[1] << "\n";
+    dataset->read(dataptr, H5::PredType::NATIVE_DOUBLE);
+    // dataset->read(dataptr, dataset->getTypeClass());
+    A = Eigen::Map<MatrixXdR>(dataptr, dims[0], dims[1]);
+    file.close();
+    std::cout << A << "\n";
 
-        file.openFile(FILE_NAME, H5F_ACC_RDONLY);
-        dataset = new H5::DataSet(file.openDataSet(DATASET_NAME));
-        dataspace = new H5::DataSpace(dataset->getSpace());
-        std::cout << "old rank: "<<rank << "\t old dims: "
-                  << dims[0] << ","<<dims[1] << "\n";
-        rank = dataspace->getSimpleExtentNdims();
-        rank = dataspace->getSimpleExtentDims(dims_ptr);
-        std::cout << "new rank: "<<rank << "\t new dims: "
-                  << dims[0] << ","<<dims[1] << "\n";
-        file.close();
-        delete dataspace;
-        delete dataset;
-        dataptr = NULL;
-
-    }  // end of try block
-    // catch failure caused by the H5File operations
-    catch(H5::FileIException error) {
-        error.printError();
-        return -1;
-    }
-
-    // catch failure caused by the DataSet operations
-    catch(H5::DataSetIException error) {
-        error.printError();
-        return -1;
-    }
-
-    // catch failure caused by the DataSpace operations
-    catch(H5::DataSpaceIException error) {
-        error.printError();
-        return -1;
-    }
+    delete dataspace;
+    delete dataset;
+    dataptr = NULL;
 
     return 0;  // successfully terminated
 }
