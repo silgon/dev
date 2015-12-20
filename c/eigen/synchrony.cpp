3dc3a1aaab9c33ba6956f81feb97c644475f4a7c
silgon <silgon3200@gmail.com>
2015-12-21 00:37:12 +0100

    synchrony

--- c/eigen/synchrony.cpp
+++ c/eigen/synchrony.cpp
@@ -9,9 +9,15 @@ int main(int argc, char *argv[])
     Eigen::MatrixXd centered = A.colwise() - A.rowwise().mean();
     Eigen::MatrixXd cov = (centered*centered.adjoint()) / double(A.cols() - 1);
 
+    Eigen::VectorXcd eig =cov.eigenvalues();
+    Eigen::MatrixXd lambda_spectrum = eig.real()/cov.trace();
+    double SI;
+    SI= 1+(lambda_spectrum.cwiseProduct(lambda_spectrum.array().log().matrix())).sum()/std::log(lambda_spectrum.rows());
+
     std::cout << "Matrix A:\n"<< A << "\n";
     std::cout <<  "difference with mean (rows):\n"<< centered << "\n";
     std::cout <<  "covariance:\n"<< cov << "\n";
+    std::cout << "SI: " << SI << "\n";
 
     return 0;
 }
@@ -19,5 +25,9 @@ int main(int argc, char *argv[])
 # numpy test
 import numpy as np
 A = np.array([[1,2,3,4],[5,6,7,8]])
-np.cov(A)
+covar= np.cov(A)
+trace_covar = np.trace(covar)
+eigval, eigvec = np.linalg.eig(covar)
+lambda_spectrum = eigval/trace_covar
+SI = 1 + sum(lambda_spectrum*np.log(lambda_spectrum))/np.log(len(lambda_spectrum))
 */
