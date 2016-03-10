5c92ac3d5c1a0ceb919be91b7c7b9e14fa688536
silgon <silgon3200@gmail.com>
2016-03-10 21:26:47 +0100

    ipopt examples

---
+++ c/optimization/ipopt/ipopt_example/FindIPOPT.cmake
@@ -0,0 +1,47 @@
+# includes
+SET(POSSIBLE_INC_PATHS 
+  /usr/include
+  /usr/include/coin
+  /usr/include/coin/ThirdParty
+  /usr/local/include
+  /usr/local/include/coin
+  /usr/local/include/coin/ThirdParty
+  )
+FIND_PATH(IPOPT_INCLUDE_DIR NAMES IpIpoptApplication.hpp defs.h
+  PATHS
+  ${POSSIBLE_INC_PATHS}
+)
+FIND_PATH(IPOPT_THIRDPARTY_INCLUDE_DIR NAMES defs.h
+  PATHS
+  ${POSSIBLE_INC_PATHS}
+)
+
+SET(IPOPT_INCLUDE_DIRS ${IPOPT_INCLUDE_DIR} ${IPOPT_THIRDPARTY_INCLUDE_DIR})
+
+set(IPOPT_CXX_FLAGS "-DHAVE_CSTDDEF")
+
+# libraries
+SET(POSSIBLE_LIB_PATHS
+  /lib/
+  /usr/lib/
+  /usr/local/lib 
+  /usr/lib/gcc/x86_64-linux-gnu/4.8 
+  /usr/lib/x86_64-linux-gnu 
+  /opt/local/lib
+  )
+
+SET(LIBS ipopt lapack dl coinmumps blas gfortran m quadmath coinmetis)
+FOREACH (LIB ${LIBS})
+  FIND_LIBRARY(FOUND_${LIB} NAMES ${LIB}
+    PATHS ${POSSIBLE_LIB_PATHS})
+  SET(IPOPT_LIBRARIES ${IPOPT_LIBRARIES} ${FOUND_${LIB}})
+ENDFOREACH(LIB)
+
+IF (IPOPT_INCLUDE_DIRS AND IPOPT_LIBRARIES)
+  SET(IPOPT_FOUND TRUE)
+  MESSAGE(STATUS "IPOPT found")
+  MESSAGE(STATUS "IPOPT Include dirs: " ${IPOPT_INCLUDE_DIRS})
+  MESSAGE(STATUS "IPOPT Libraries: " ${IPOPT_LIBRARIES})
+ELSE (IPOPT_INCLUDE_DIRS AND IPOPT_LIBRARIES)
+  MESSAGE(STATUS "IPOPT was not found")
+ENDIF(IPOPT_INCLUDE_DIRS AND IPOPT_LIBRARIES)
