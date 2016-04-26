c0ebaba1352d11c65ffb59fe58068c24ed83a216
silgon <silgon3200@gmail.com>
2016-04-26 10:50:56 +0200

    correction to point_to_line in julia

--- julia/point_to_line.jl
+++ julia/point_to_line.jl
@@ -1,4 +1,4 @@
-p = [2,2]
+p = [2, 2]
 #ax+by+c=0
 a=3
 b=2
@@ -7,6 +7,7 @@ result = abs(a*p[1]+b*p[2]+c)/sqrt(a^2+b^2)
 println(result)
 
 # y=bx+c
+# actual equation y=-bbx-cc
 bb = a/b
 cc = c/b
 result2 = abs(p[1]*bb+p[2]+cc)/sqrt(1+bb^2)
