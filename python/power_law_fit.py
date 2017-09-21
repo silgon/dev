a182e7c9a4530988c0f2225a6f057208815ecb62
silgon <silgon3200@gmail.com>
2017-09-21 16:39:45 +0200

    plot information

--- python/power_law_fit.py
+++ python/power_law_fit.py
@@ -2,6 +2,25 @@ from scipy.stats import powerlaw
 import matplotlib.pyplot as plt
 import numpy as np
 
-pl=powerlaw(.3, loc=0.3, scale=2)
+pl=powerlaw(.8, loc=0, scale=2)
 samples = pl.rvs(10000) # create random variables
 alpha, loc, scale = powerlaw.fit(samples) # fit the variables
+
+# plotting
+plt.figure(0)
+plt.clf()
+plt.hist(samples, bins=50, normed=True, histtype='stepfilled', alpha=.9)
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+plt.plot(x, pl.pdf(x), linewidth=2, label="fit")
+
+plt.figure(1)
+plt.clf()
+plt.hist(samples, bins=50, normed=True, histtype='stepfilled', alpha=.9)
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+plt.plot(x, pl.pdf(x), linewidth=2, label="fit")
+plt.xscale("log", basex=10,nonposy='clip')
+plt.yscale("log", basey=10,nonposy='clip')
+
+plt.show()
