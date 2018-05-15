00c5c121257080e6c1beadc3b20b91429c53a59c
silgon <silgon3200@gmail.com>
2018-05-15 17:21:17 +0200

    add: not longer continuous

--- python/mingame_continuous.py
+++ python/mingame_continuous.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def win(p1, p_arr):
+    """ get that p1 (float) gets given an array of points p_arr(numpy.array)
+    """
     if p1 in p_arr:
         return 0 # this case should never enter
     c = p_arr<p1 # condition
@@ -16,28 +18,29 @@ def win(p1, p_arr):
         p_r =(t_+p1)/2
     return p_r-p_l
 
-x = np.array([.1,.7,.9])
-res = [win(x[i],np.delete(x,i)) for i in range(len(x))]
+# next code might be used later
+# x = np.array([.1,.7,.9])
+# res = [win(x[i],np.delete(x,i)) for i in range(len(x))]
 
-import matplotlib.pyplot as plt
-P = 2
-N = 50
-tmp = np.linspace(0,1,N)
-# X, Y = np.meshgrid(tmp, tmp)
-# Z = np.zeros_like(X)
-# for i in range(N):
-#     for j in range(N):
-#         Z[i,j] = win(X[i,j], np.array([Y[i,j]]))
-# plt.pcolor(X, Y, Z)
-# plt.colorbar()
-# plt.show()
-W = np.zeros([N, N, P])
+P = 3 # number of points
+N = 50 # number of samples (discretized problem)
+tmp = np.linspace(0,1,N) # sample space
+
+W = np.zeros([N, N, N, P])
 for n1 in range(N):
     for n2 in range(N):
-        W[n1,n2,0] = win(tmp[n1],np.array([tmp[n2]]))
-        W[n1,n2,1] = win(tmp[n2],np.array([tmp[n1]]))
+        for n3 in range(N):
+            W[n1,n2,n3,0] = win(tmp[n1],tmp[[n2,n3]])
+            W[n1,n2,n3,1] = win(tmp[n2],tmp[[n1,n3]])
+            W[n1,n2,n3,2] = win(tmp[n3],tmp[[n1,n2]])
 
-p2_am = np.argmax(W[:,:,1],axis=1)
-p1_am = np.argmax([W[n1,p2_am[n1],0] for n1 in range(N)])
-print(W[p1_am,p2_am[p1_am]])
+# find optimal values of P3 for all combinations of P2 and P1
+p3_am = np.argmax(W[:,:,:,2],axis=2)
+# find optimal values of P2 given max P3 for all combinations of P1
+p2_am = np.argmax([[W[n1,n2,p3_am[n1,n2],1] for n2 in range(N)] for n1 in range(N)],axis=1)
+# find optimal value of P1 max P2 given max P3
+p1_am = np.argmax([W[n1,p2_am[n1],p3_am[n1,p2_am[n1]],0] for n1 in range(N)])
+i1,i2,i3 = p1_am, p2_am[p1_am], p3_am[p1_am, p2_am[p1_am]]
+positions = tmp[[i1,i2,i3]]
+place = W[i1,i2,i3]
 
