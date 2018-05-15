f431f69be8e355446f25f98e483f6ebfaea87e01
silgon <silgon3200@gmail.com>
2018-05-15 16:10:13 +0200

    add: continuous more stuff (not really working)

--- python/mingame_continuous.py
+++ python/mingame_continuous.py
@@ -20,17 +20,20 @@ x = np.array([.1,.7,.9])
 res = [win(x[i],np.delete(x,i)) for i in range(len(x))]
 
 import matplotlib.pyplot as plt
-n_samples = 50
-tmp = np.linspace(0,1,n_samples)
-X, Y = np.meshgrid(tmp, tmp)
-Z = np.zeros_like(X)
-for i in range(n_samples):
-    for j in range(n_samples):
-        Z[i,j] = win(X[i,j], np.array([Y[i,j]]))
-plt.pcolor(X, Y, Z)
-plt.colorbar()
-plt.show()
+P = 2
+N = 50
+tmp = np.linspace(0,1,N)
+# X, Y = np.meshgrid(tmp, tmp)
+# Z = np.zeros_like(X)
+# for i in range(N):
+#     for j in range(N):
+#         Z[i,j] = win(X[i,j], np.array([Y[i,j]]))
+# plt.pcolor(X, Y, Z)
+# plt.colorbar()
+# plt.show()
+W = np.zeros([N, N, P])
+for n1 in range(N):
+    for n2 in range(N):
+        W[n1,n2,0] = win(tmp[n1],np.array([tmp[n2]]))
+        W[n1,n2,1] = win(tmp[n2],np.array([tmp[n1]]))
 
-def ob(x):
-    d=0
-    return d
